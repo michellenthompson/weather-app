2fa3c91e789dd7239e3c0957243be7bed5837a40
michellenthompson <michellenthomp@gmail.com>
2023-05-16 19:54:40 +0200

    Started adding an API for forecast function

--- src/index.js
+++ src/index.js
@@ -26,7 +26,8 @@ function formatDate() {
 formatDate();
 
 //Forecast
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -52,7 +53,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-displayForecast();
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
 
 //Search and current weather with API
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -103,6 +109,8 @@ function showTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 //Current city
